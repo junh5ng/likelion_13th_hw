fdbe3d8316025255234e14c2606c2f495aa088a8
junh5ng <wnsghd1130@dgu.ac.kr>
2025-05-06 23:56:29 +0900

    final

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+  title = models.CharField(max_length=100)
+  author = models.CharField(max_length=50)
+  body = models.TextField()
+  category = models.CharField(max_length=20)
+  image = models.ImageField(upload_to="post_images/", blank=True, null=True)
+
+  def __str__(self):
+    return self.title
+  
+  def summary(self):
+    return self.body[:10]
--- main/views.py
+++ main/views.py
@@ -1,12 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+
+from .models import *
 
 # Create your views here.
+
+print(Post.objects.all())
 
 def mainpage(request):
   return render(request, 'main/mainpage.html')
 
 def secondpage(request):
-  return render(request, 'main/secondpage.html')
+  posts = Post.objects.all()
+  return render(request, 'main/secondpage.html', {'posts': posts})
 
 def mainpage(request):
   context = {
@@ -23,3 +28,44 @@ def mainpage(request):
     ]
   }
   return render(request, 'main/mainpage.html', context)
+
+def new_post(request):
+    return render(request, 'main/new-post.html')
+
+
+def detail(request, id):
+    post = get_object_or_404(Post, pk=id)
+    return render(request, 'main/detail.html', {'post': post})
+
+
+def edit(request, id):
+    edit_post = Post.objects.get(pk=id)
+    return render(request, 'main/edit.html', {"post": edit_post})
+
+
+def create(request):
+  new_post = Post()
+  new_post.title = request.POST['title']
+  new_post.author = request.POST['author']
+  new_post.body = request.POST['body']
+  new_post.category = request.POST['category']
+  new_post.image = request.FILES.get('image')
+  
+  new_post.save()
+  return redirect('main:detail', new_post.id)
+
+def update(request, id):
+  update_post = Post.objects.get(pk=id)
+  update_post.title = request.POST['title']
+  update_post.author = request.POST['author']
+  update_post.body = request.POST['body']
+  update_post.category = request.POST['category']
+  update_post.image = request.FILES.get('image')
+
+  update_post.save()
+  return redirect('main:detail', update_post.id)
+
+def delete(request, id):
+  delete_post = Post.objects.get(pk=id)
+  delete_post.delete()
+  return redirect('main:secondpage')
