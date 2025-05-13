385cbfa942c3b543458dcbaf2ef87762fa28a0c8
junh5ng <wnsghd1130@dgu.ac.kr>
2025-05-13 23:06:59 +0900

    5th session hw

---
+++ users/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from django.contrib import auth
+from django.contrib.auth.models import User
+from main.models import Post
+from accounts.models import Profile
+
+# Create your views here.
+def mypage(request, id):
+  user=get_object_or_404(User, pk=id)
+  profile = Profile.objects.get(user=user)
+  posts = Post.objects.filter(author=user)
+
+  context = {
+    'profile': profile,
+    'posts': posts,
+  }
+  return render(request, 'users/mypage.html', context)
