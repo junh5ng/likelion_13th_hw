636f7334125082a5c9f808a8816f4bb26e7966f8
junh5ng <wnsghd1130@dgu.ac.kr>
2025-05-14 00:04:35 +0900

    post 모델 수정

--- main/models.py
+++ main/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
   title = models.CharField(max_length=100)
-  author = models.CharField(max_length=50)
+  # author = models.CharField(max_length=50)
+  author = models.ForeignKey(User, on_delete = models.CASCADE)
   body = models.TextField()
   category = models.CharField(max_length=20)
   image = models.ImageField(upload_to="post_images/", blank=True, null=True)
