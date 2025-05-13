eba2e92a7302fe5577652d0d4658a85afbd4b0d4
junh5ng <wnsghd1130@dgu.ac.kr>
2025-05-13 23:58:51 +0900

    5th session hw_second

--- users/views.py
+++ users/views.py
@@ -6,12 +6,13 @@ from accounts.models import Profile
 
 # Create your views here.
 def mypage(request, id):
-  user=get_object_or_404(User, pk=id)
+  user = get_object_or_404(User, pk=id)
   profile = Profile.objects.get(user=user)
+  
   posts = Post.objects.filter(author=user)
 
   context = {
-    'profile': profile,
-    'posts': posts,
+      'profile': profile,
+      'posts': posts,
   }
   return render(request, 'users/mypage.html', context)
